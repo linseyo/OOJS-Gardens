0377c836122039f81db29083ce5558faa8368652
Austin Kading & Dillon Estelle <pair+austin-kading.destelle@devbootcamp.com>
2017-04-11 10:26:14 -0700

    adds remove method

--- src/garden.js
+++ src/garden.js
@@ -12,9 +12,16 @@ var garden = {
   addFlower: function(flower) {
     this.flowers.push(flower);
   },
-plant: function(planted) {
+  plant: function(planted) {
     for(var i = 0; i < planted.length; i++){
       this.flowers.push(planted[i]);
+    }
+  },
+  remove: function(flower) {
+    for(var i = 0; i < this.flowers.length; i++){
+      if(this.flowers[i] == flower){
+        this.flowers.splice(i, 1);
+      }
     }
   }
 }
