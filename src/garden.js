2b7479af14f532e688d8a16f31d87ce03b3a0b95
Matthew Fischer <email": null>
2017-04-11 10:17:28 -0700

    Write remove function for garden

--- src/garden.js
+++ src/garden.js
@@ -9,4 +9,20 @@ var garden = {
   name: "Kula Botanical Garden",
   location: "Makawao",
   flowers: [],
+  addFlower: function(flower){
+    this.flowers.push(flower);
+  },
+  plant: function(flowers){
+    for (var i = 0; i < flowers.length; i++){
+      this.flowers.push(flowers[i]);
+    };
+  },
+  remove: function(flower){
+    for (var i = 0; i < this.flowers.length; i++){
+      if (this.flowers[i] === flower){
+        this.flowers.splice(i, 1);
+      };
+    };
+  },
+
 }
