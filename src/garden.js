8b3cea8d38ed91191260cf867359ff82edc4280f
Linsey R <email": null>
2017-04-11 10:51:47 -0700

    Write function to match flowers by color

--- src/garden.js
+++ src/garden.js
@@ -5,4 +5,27 @@ the attributes and behaviors described in spec/garden_spec.js.
 
 */
 
-var garden = {}
+var garden = {
+  name: "Kula Botanical Garden",
+  location: "Makawao",
+  flowers: []
+}
+
+garden.addFlower = function(newFlower) {
+  this.flowers.push(newFlower);
+};
+
+garden.plant = function([flowerOne, flowerTwo]) {
+  this.flowers.push(flowerOne, flowerTwo);
+};
+
+garden.remove = function(flower) {
+  this.flowers.pop(flower);
+  return this.flowers;
+};
+
+garden.flowersByColor = function(color) {
+  return this.flowers.filter( function(flower) {
+    return flower.color === color;
+  }, color);
+}
