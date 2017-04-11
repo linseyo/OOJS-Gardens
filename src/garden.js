419e79ef98f329237b504739dc0fa5d9a1335ad0
Nick Jarin <email": null>
2017-04-11 10:53:50 -0700

    Add properties to garden object literal, pass all tests

--- src/garden.js
+++ src/garden.js
@@ -5,4 +5,41 @@ the attributes and behaviors described in spec/garden_spec.js.
 
 */
 
-var garden = {}
+var garden = {
+  name: "Kula Botanical Garden",
+  location: "Makawao",
+  flowers: [],
+  flowerPopulation: function(){
+    return this.flowers.length;
+  },
+  addFlower: function(flower){
+    this.flowers.push(flower);
+  },
+  plant: function(flowers){
+    for(var i = 0; i < flowers.length; i++){
+      this.addFlower(flowers[i]);
+    }
+  },
+  remove: function(flower){
+    var deleteIndex = this.flowers.indexOf(flower);
+    this.flowers.splice(deleteIndex);
+  },
+  flowersByColor: function(soughtColor){
+    var result = [];
+    for(var i = 0; i < this.flowers.length; i++){
+      if(this.flowers[i].color === soughtColor){
+        result.push(this.flowers[i]);
+      }
+    }
+    return result;
+  },
+  flowersByName: function(soughtName){
+    var result = [];
+    for(var i = 0; i < this.flowers.length; i++){
+      if(this.flowers[i].name === soughtName){
+        result.push(this.flowers[i]);
+      }
+    }
+    return result;
+  },
+}
