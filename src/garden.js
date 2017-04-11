33e5609d9387dd6f6fed2cde416210261a9fbc27
Julia(e) Xu <email": null>
2017-04-11 10:32:24 -0700

    Add methods to garden object literal

--- src/garden.js
+++ src/garden.js
@@ -5,9 +5,47 @@ the attributes and behaviors described in spec/garden_spec.js.
 
 */
 
+
 // OBJECT LITERAL SYNTAX
 var garden = {
   name: "Kula Botanical Garden",
   location: "Makawao",
-  flowers: []
+  flowers: [],
+
+  addFlower: function(flower) {
+    this.flowers.push(flower);
+  },
+
+  plant: function(multipleFlowers) {
+    this.flowers = this.flowers.concat(multipleFlowers);
+  },
+
+  remove: function(flower) {
+
+    for (var i = 0; i < this.flowers.length; i++) {
+      if ((this.flowers[i].name === flower.name) && (this.flowers[i].color === flower.color)) {
+        this.flowers.splice(i, 1);
+      }
+    }
+  },
+
+  flowersByColor: function(color) {
+    var sameColor = [];
+    for (var i = 0; i < this.flowers.length; i++) {
+      if (this.flowers[i].color === color) {
+        sameColor.push(this.flowers[i]);
+      }
+    }
+    return sameColor;
+  },
+
+  flowersByName: function(name) {
+    var sameName = [];
+    for (var i = 0; i < this.flowers.length; i++) {
+      if (this.flowers[i].name === name) {
+        sameName.push(this.flowers[i]);
+      }
+    }
+    return sameName;
+  },
 }
