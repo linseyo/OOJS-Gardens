cbc5c7627e339658e79798d84b637cfffad3f670
Philip Cornman <pjc5108@gmail.com>
2017-04-11 15:28:50 -0700

    pass all spec tests

--- src/garden.js
+++ src/garden.js
@@ -5,4 +5,29 @@ the attributes and behaviors described in spec/garden_spec.js.
 
 */
 
-var garden = {}
+var garden = {
+	name: "Kula Botanical Garden",
+	location: "Makawao",
+	flowers: [],
+	addFlower: function(flower){
+		garden.flowers.push(flower);
+	},
+	plant: function(flowersToPlant){
+		flowersToPlant.forEach(garden.addFlower);
+	},
+	remove: function(flower){
+		flower_index = garden.flowers.indexOf(flower);
+		garden.flowers.splice(flower_index, 1);
+	},
+	flowersByColor: function(filterColor){
+		return garden.flowers.filter(function(flower){
+			return flower.color == filterColor;
+		});
+	},
+	flowersByName: function(flowerName){
+		return garden.flowers.filter(function(flower){
+			return flower.name == flowerName;
+		});
+	}
+
+};
