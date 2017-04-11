9cbd95f90ef1675581efaa6a1b661c450648cc36
Dan McKeon & Eitan Greene <pair+danmckeon.eitan-g@devbootcamp.com>
2017-04-11 10:56:59 -0700

    write garden properties and functions

--- src/garden.js
+++ src/garden.js
@@ -5,4 +5,28 @@ the attributes and behaviors described in spec/garden_spec.js.
 
 */
 
-var garden = {}
+var garden = {
+  name: "Kula Botanical Garden",
+  location: "Makawao",
+  flowers: [],
+  addFlower: function(flower) {
+    this.flowers.push(flower);
+  },
+  plant: function(flowers) {
+    this.flowers = this.flowers.concat(flowers);
+  },
+  remove: function(flower) {
+    var flowerIndex = this.flowers.indexOf(flower);
+    this.flowers.splice(flowerIndex, 1);
+  },
+  flowersByColor: function(color) {
+    return this.flowers.filter(function(flower) {
+      return flower.color === color;
+    });
+  },
+  flowersByName: function(name) {
+    return this.flowers.filter(function(flower) {
+      return flower.name === name;
+    });
+  }
+};
