70442271dc74696311c9ea726936e130c93a405f
Jordan Levine <email": null>
2017-04-11 16:20:39 -0700

    Remove flower

--- src/garden.js
+++ src/garden.js
@@ -14,7 +14,7 @@ garden.flowers = [];
 garden.plant = function(flowers) {
   var i;
   for(i=0; i < flowers.length; i++) {
-    newFlower = flowers[i];
+    var newFlower = flowers[i];
     console.log('newFlower is ',newFlower)
     this.addFlower(newFlower);
   }
@@ -22,6 +22,11 @@ garden.plant = function(flowers) {
 
 garden.addFlower = function(flower) {
     this.flowers.push(flower);
+}
+
+garden.remove = function(flower) {
+  var flowerIndex = this.flowers.indexOf(flower);
+  this.flowers.splice(flowerIndex,1);
 }
 
 
