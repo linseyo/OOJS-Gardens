3d18683accd6125addc1b0c7aac8af3eaf7e9489
Jordan Levine <email": null>
2017-04-11 14:11:45 -0700

    Create flower construction function and some instances

--- src/flower.js
+++ src/flower.js
@@ -5,4 +5,13 @@ matching the description in spec/flower_spec.js.
 
 */
 
-var Flower = function() {}
+var Flower = function(name,color) {
+  this.name = name;
+  this.color = color;
+}
+
+var flower1 = new Flower('rose','red');
+var flower2 = new Flower('daisy','white');
+var flower3 = new Flower('tulip','pink');
+console.log('flower 1 to follow');
+console.log(flower1);
